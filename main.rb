b36a73e6b6701a97a3935bae53a38d33a6335cd9
Corey Leveen <mntj11@gmail.com>
2015-04-18 15:08:10 -0400

    Add sessions

--- main.rb
+++ main.rb
@@ -3,11 +3,14 @@ Bundler.require
 
 require_relative 'config.rb'
 
+enable :sessions
+
 get '/' do
   "Welcome to Montext!"
 end
 
 get '/inbound' do
+  session["count"] ||= 0
   body = params["Body"]
 
   twiml = Twilio::TwiML::Response.new do |r|
@@ -15,21 +18,30 @@ get '/inbound' do
       msg.Body create_reply(body)
     end
   end
+
+  session["count"] += 1
   twiml.text
 end
 
 def create_reply(input)
-  i = input
-
-  if i.strip.downcase == ""
+  if session["count"] == 0
+    new_message << help_message
+  elsif input.strip.downcase == "commands"
     help_message
   else
-    "Sorry! Didn't recognize that. " +
-    help_message
+    error_message << help_message
   end
+end
+
+def new_message
+  "Welcome to Montext! "
 end
 
 def help_message
   "Enter a stock symbol to get information about it. " <<
   "For example, 'GOOG'"
+end
+
+def error_message
+  "Sorry! Didn't recognize that. "
 end
