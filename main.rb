7cf75204f57736d905c8c68ac2693714362e51c0
Corey Leveen <mntj11@gmail.com>
2015-04-18 14:13:05 -0400

    Basic response filtering

--- main.rb
+++ main.rb
@@ -4,12 +4,32 @@ Bundler.require
 require_relative 'config.rb'
 
 get '/' do
-  "Welcome to Montext"
+  "Welcome to Montext!"
 end
 
-get '/sms-reply' do
+get '/inbound' do
+  body = params["Body"]
+
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Heyman"
+    r.Message do |msg|
+      msg.Body create_reply(body)
+    end
   end
   twiml.text
+end
+
+def create_reply(input)
+  i = input
+
+  if i.strip.downcase == ""
+    help_message
+  else
+    "Sorry! Didn't recognize that. " +
+    help_message
+  end
+end
+
+def help_message
+  "Enter a stock symbol to get information about it. " <<
+  "For example, 'GOOG'"
 end
